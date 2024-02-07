9f86afef62a98a7db9adbe5399da6dd9e4624efa
= <=>
2024-02-07 12:17:47 +0500

    Added more examples

--- functions.py
+++ functions.py
@@ -1,2 +1,27 @@
-def hello(args):
-    print("Hello, World!", args)
+def hello(name):
+    print("Hello,", name)
+
+
+def while_loop():
+    i = 0
+    while True:
+        print(i, end=', ')
+        i += 1
+
+        if i >= 10:
+            break
+    print()
+
+def for_loop():
+    for i in range(10):
+        print(i, end=', ')
+    print()
+
+
+def conditions(n):
+    if n > 5:
+        print(f'{n} > 5')
+    elif n < 5:
+        print(f'{n} < 5')
+    else:
+        print(f'{n} == 5')
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 """ The main module. """
-from functions import hello
+import functions as fn
 
 
 def main():
     """ The main function. """
-    hello('\n- Farooq')  # String can be inside both single or double quotes
-    for i in range(0, 10):
-        print(i, end=' ')
-    print()
+    name = input('What is your name? ')
+    fn.hello(name)  # String can be inside both single or double quotes
+    
+    print('For Loop:')
+    fn.for_loop()
 
+    print('While Loop:')
+    fn.while_loop()
+
+    n = input('Enter a number between 0 and 10: ')
+
+    try:
+        n = int(n) # try to cast n to integer
+        fn.conditions(n)
+    except ValueError:
+        print('Please enter a digit')
 
 if __name__ == '__main__':
     main()
